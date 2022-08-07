a36500943d3e8565b69f13a5ee55691621c7f269
Fedor <farlep1970@yandex.ru>
2022-08-07 23:27:45 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Определение max и min из двух чисел");
+Console.Write("Введите первое число: ");
+string numberString1 = Console.ReadLine();
+int number1 = int.Parse(numberString1);
+Console.Write("Введите второе число: ");
+string numberString2 = Console.ReadLine()!;
+int number2 = int.Parse(numberString2);
+if (number1 > number2)
+    {Console.WriteLine("Max = " + number1);
+     Console.WriteLine("Min = " + number2);}
+else if (number1 < number2)
+    {Console.WriteLine("Max = " + number2);
+     Console.WriteLine("Min = " + number1);}
+else {Console.WriteLine("Числа равны");}
