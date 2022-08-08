65f0cf5ab46f3be4cf0b69a3f0d0dc7d2b48e5e4
Fedor <farlep1970@yandex.ru>
2022-08-08 22:29:53 +0300

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Определение max из трех чисел");
+Console.Write("Введите первое число: ");
+string? numberString1 = Console.ReadLine();
+int number1 = int.Parse(numberString1!);
+Console.Write("Введите второе число: ");
+string numberString2 = Console.ReadLine()!;
+int number2 = int.Parse(numberString2);
+Console.Write("Введите третье число: ");
+string numberString3 = Console.ReadLine()!;
+int number3 = int.Parse(numberString3);
+int max = number1;
+if (number2 > max)
+    {max = number2;}
+else if (number3 > max)
+    {max = number3;}
+Console.WriteLine("Max = " + max);
