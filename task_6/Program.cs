99b1c477e7aa59b462fd03be18ff0fa839fbb9cf
Fedor <farlep1970@yandex.ru>
2022-08-08 22:43:15 +0300

    task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Проверка число на четность");
+Console.Write("Введите число: ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+if (number%2 == 0)
+    {Console.WriteLine("Число четное");}
+else {Console.WriteLine("Число нечетное");}
