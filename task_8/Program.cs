dd5fb8cb608e8cac5951c327fbc6ac2eec96b1c7
Fedor <farlep1970@yandex.ru>
2022-08-09 15:33:10 +0300

    task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Вывод всех четных чисел в диаппазоне от 1 до N");
+Console.Write("Введите N: ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+int i = 2;
+if (number < 2)
+    {Console.WriteLine("Число должно быть больше 1");}
+else {while (i <= number)
+        {Console.Write(i + " ");
+         i = i + 2;}
+}
+Console.WriteLine();
